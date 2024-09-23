1a6b1061cb501581eb9d71dcbf41128a8e608a4c
Manuel Lagos <lagosmanual@gmail.com>
2024-09-23 18:44:21 -0300

    fix another bug

--- component.js
+++ component.js
@@ -1,8 +1,8 @@
 class Component extends HTMLElement {
     constructor() {
-        super()
-        this.innerHTML = `<p>hola mundo</p>`
+        super();
+        this.innerHTML = `<p>hola mundo</p>`;
     }
 }
 
-customElements.define("team-abc", Component)
+customElements.define("team-abc", Component);
